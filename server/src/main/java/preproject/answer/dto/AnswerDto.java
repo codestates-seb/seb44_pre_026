bf7926a16643ba046923f93a7c449d86ab72862b
[GwanYong Jung] <[valid-email]>
2023-06-16 13:33:01 +0900

    feat: Add Answer dto

---
+++ server/src/main/java/preproject/answer/dto/AnswerDto.java
@@ -0,0 +1,36 @@
+package preproject.answer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+public class AnswerDto {
+
+    @Getter
+    @AllArgsConstructor
+    public static class Post {
+        @NotBlank
+        private String content;
+    }
+
+    @Getter
+    public static class Patch {
+        private long answerId;
+        @NotBlank
+        private String content;
+    }
+
+    @Getter
+    @Builder
+    public static class Response {
+        private Long answerId;
+        private Long questionId;
+        private Long userId;
+        private String content;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+    }
+}
