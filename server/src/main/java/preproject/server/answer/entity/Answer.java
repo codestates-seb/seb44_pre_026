c0ef172832e557a3e184f11df7b726c4f4bac87e
[GwanYong Jung] <[valid-email]>
2023-06-16 15:56:13 +0900

    fix: ReUpload Answer entitiy

--- server/src/main/java/preproject/server/answer/entity/Answer.java
+++ server/src/main/java/preproject/server/answer/entity/Answer.java
@@ -1,4 +1,30 @@
-package preproject.answer.entity;
+package preproject.server.answer.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ANSWER_ID")
+    private long answerId;
+    @Column(nullable = false)
+    private String content;
+    @Column(nullable = false, updatable = false, name = "CREATED_AT")
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @Column(nullable = false, name = "MODIFIED_AT")
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
 }
