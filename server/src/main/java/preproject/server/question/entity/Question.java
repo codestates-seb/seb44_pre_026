0f81fcbdc8e11fa4f2731d19175d6e39b55028fa
insuko <insuko63@naver.com>
2023-06-16 14:32:37 +0900

    feat.Add Question entity

---
+++ server/src/main/java/preproject/server/question/entity/Question.java
@@ -0,0 +1,44 @@
+package preproject.server.question.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long questionId;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false, columnDefinition="TEXT")
+    private String content;
+    @Column(nullable = false, name = "CREATED_AT")
+    private LocalDateTime createdAt = LocalDateTime.now();
+    @Column(nullable = false, name = "UPDATED_AT")
+    private LocalDateTime updatedAt = LocalDateTime.now();
+    @OneToMany(mappedBy = "question", cascade = {CascadeType.ALL})
+    private List<Answer> answerList = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public void setMember(Member member) {
+        this.member = member;
+        }
+    }
+
+    public void setAnswer(Answer answer) {
+        this.getAnswerList().add(answer);
+        }
+    }
+}
