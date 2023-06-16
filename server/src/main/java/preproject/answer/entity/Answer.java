5941c0de3476b89ce2d6f2aa30e8828cc5cd9687
[GwanYong Jung] <[valid-email]>
2023-06-16 15:18:52 +0900

    fix:Add Answer entitiy JPA mapping

--- server/src/main/java/preproject/answer/entity/Answer.java
+++ server/src/main/java/preproject/answer/entity/Answer.java
@@ -3,15 +3,28 @@ package preproject.answer.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
 public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ANSWER_ID")
     private long answerId;
+    @Column(nullable = false)
     private String content;
+    @Column(nullable = false, updatable = false, name = "CREATED_AT")
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @Column(nullable = false, name = "MODIFIED_AT")
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
 
 }
