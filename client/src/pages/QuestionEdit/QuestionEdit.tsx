9ea687e0ac838d0ad8fe18e5d9bde7490cb00d19
g4dalcom <g4dalcom@gmail.com>
2023-06-19 05:43:28 +0900

    refactor: 수정 페이지 edit tip 컴포넌트 분리

--- client/src/pages/QuestionEdit/QuestionEdit.tsx
+++ client/src/pages/QuestionEdit/QuestionEdit.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import TextEditor from "../../components/TextEditor/TextEditor";
 import * as S from "./style";
 import { Link } from "react-router-dom";
+import EditTip from "../../components/EditTip/EditTip";
 
 function QuestionEdit() {
   const [bodyValue, setBodyValue] = useState("");
@@ -33,18 +34,7 @@ function QuestionEdit() {
             </Link>
           </S.ButtonLayout>
         </S.FormLayout>
-        <S.EditTip>
-          <div>
-            <S.TipHeader>How to Edit</S.TipHeader>
-            <ul>
-              <li>Correct minor typos or mistakes</li>
-              <li>Clarify meaning without changing it</li>
-              <li>Add related resources or links</li>
-              <li>Always respect the author's intent</li>
-              <li>Don't use edits to reply the author</li>
-            </ul>
-          </div>
-        </S.EditTip>
+        <EditTip />
       </S.EditLayout>
     </S.Section>
   );
--- client/src/pages/QuestionEdit/style.ts
+++ client/src/pages/QuestionEdit/style.ts
@@ -87,39 +87,3 @@ export const ButtonLayout = styled.div`
     }
   }
 `;
-
-export const EditTip = styled.article`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 365px;
-  margin-left: 24px;
-  position: sticky;
-
-  & > div {
-    width: 100%;
-  }
-
-  & ul {
-    background-color: var(--yellow-050);
-    padding: 4px 15px;
-    border-bottom-left-radius: 3px;
-    border-bottom-right-radius: 3px;
-    border: 1px solid var(--yellow-200);
-
-    & > li {
-      font-size: 13px;
-      margin: 12px;
-    }
-  }
-`;
-
-export const TipHeader = styled.div`
-  padding: 12px 15px;
-  background-color: var(--yellow-100);
-  border-top-left-radius: 3px;
-  border-top-right-radius: 3px;
-  border: 1px solid var(--yellow-200);
-  font-weight: normal;
-`;
