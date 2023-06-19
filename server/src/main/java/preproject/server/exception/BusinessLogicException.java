1bd66db2df8b352f8cd39abc01462e30fcd1cdb9
hukabo <dlwlsdn153@gmail.com>
2023-06-19 14:24:40 +0900

    Exception 추가

---
+++ server/src/main/java/preproject/server/exception/BusinessLogicException.java
@@ -0,0 +1,13 @@
+package preproject.server.exception;
+
+import lombok.Getter;
+
+public class BusinessLogicException extends RuntimeException {
+    @Getter
+    private ExceptionCode exceptionCode;
+
+    public BusinessLogicException(ExceptionCode exceptionCode) {
+        super(exceptionCode.getStatusDescription());
+        this.exceptionCode = exceptionCode;
+    }
+}
