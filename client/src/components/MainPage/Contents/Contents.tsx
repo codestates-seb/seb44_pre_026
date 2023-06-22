5ba7a4bc5c68b0aaa90fa5a3ba5f92717c2ca64e
ShinyoungPark99 <shinyoung011399@gmail.com>
2023-06-23 02:55:41 +0900

    feat: 1차 메인페이지 구현

---
+++ client/src/components/MainPage/Contents/Contents.tsx
@@ -0,0 +1,17 @@
+import * as S from "./style";
+
+function Contents() {
+    return (
+        <Content>
+            <ContentHead>
+                <UpperPart/>
+                <DownerPart dataAmount = {dataAmount} />
+            </ContentHead>
+            <ContentBody>
+
+            </ContentBody>
+        </Content>
+    );
+}
+
+export default Contents;
