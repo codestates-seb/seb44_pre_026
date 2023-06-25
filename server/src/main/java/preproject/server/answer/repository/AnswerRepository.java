d275947f09537a82a16424a59ea55275cc6a30b9
insuko <124853192+insuko@users.noreply.github.com>
2023-06-25 14:01:10 +0900

    Delete server directory

--- server/src/main/java/preproject/server/answer/repository/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package preproject.server.answer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import preproject.server.answer.entity.Answer;
-
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-}
